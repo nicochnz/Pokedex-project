605e5c3b802f5dd2985c77e1bcc5fe9d1896b510
nicochnz <chiche.nicolas@gmail.com>
2024-10-23 16:10:24 +0200

    third quest react

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,27 @@
 function PokemonCards() {
-  return (
-    <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
-    </figure>
-  );
+	const pokemon = pokemonList[0];
+	return (
+		<figure>
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={pokemon.name} />
+			) : (
+				<p>???</p>
+			)}
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	);
 }
+
+const pokemonList = [
+	{
+		name: "bulbasaur",
+
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+
+	{
+		name: "mew",
+	},
+];
 export default PokemonCards;
